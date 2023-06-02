a97df7ef8d4cce5044398a335a7121b2c5576778
Leandro Prudente <lfprudente@ufg.br>
2023-06-02 14:25:03 -0300

    Update ProxGrad.m

--- ProxGrad.m
+++ ProxGrad.m
@@ -44,7 +44,7 @@ while(1)
 
     % Solve the subproblem
 
-    [theta,p,flagIS] = subproblemProxGrad(n,m,l,u,x,JH,G,dimA,A,b);
+    [theta,tau,p,flagIS] = subproblemProxGrad(n,m,l,u,x,JH,G,dimA,A,b);
 
     % Print information
 
@@ -116,7 +116,7 @@ while(1)
 
     % Compute the step size
 
-    [stp,H,G,nfevLS,flagLS] = armijo(n,m,x,d,F,JH,theta,ftol,A,b);
+    [stp,H,G,nfevLS,flagLS] = armijo(n,m,x,d,F,JH,tau,ftol,A,b);
     nfev = nfev + nfevLS;
 
     %stp = min(1,abs(theta)/(2* norm(p-x)^2));
